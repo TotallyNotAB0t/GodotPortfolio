ecabd49dc4388542d1f7ea41707a55b1242b25c0
Pierre Durand <85309955+TotallyNotAB0t@users.noreply.github.com>
2022-09-05 09:12:44 +0200

    Modified Timeline
    Added a better animation, fade to do next

--- Scripts/ProgressBar.gd
+++ Scripts/ProgressBar.gd
@@ -2,16 +2,23 @@ extends Control
 
 onready var timer
 onready var progress_bar
+onready var title
+onready var content
 
 
 func _ready():
 	timer = $Timer
 	progress_bar = $ProgressBar
+	title = $TitleContainer
+	content = $ContentContainer
 
 func _on_Timer_timeout():
 	progress_bar.value += 1
 	if(progress_bar.value == 100):
 		timer.stop()
+	if(progress_bar.value == 50):
+		title.show()
+		content.show()
 
 
 func _on_Area2D_area_entered(_area):
