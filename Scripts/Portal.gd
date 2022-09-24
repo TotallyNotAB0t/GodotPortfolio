5a22a7bf0b09dad7fda26d4fa6925683c4505017
Pierre Durand <pierre.durand95360@gmail.com>
2022-09-24 10:31:37 +0200

    new animation wip

--- Scripts/Portal.gd
+++ Scripts/Portal.gd
@@ -2,13 +2,14 @@ extends Node2D
 
 var can_tp = false
 export (String, FILE) var next_scene
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+export (NodePath) var player_node
 
 func Teleport():
 	var _error = get_tree().change_scene(next_scene)
+
+func play_despawn_animation():
+	player_node.find_node(AnimatedSprite).play("despawn")
+	return player_node.find_node(AnimatedSprite)
 
 func _on_Area2D_area_entered(_area):
 	can_tp = true
@@ -18,4 +19,5 @@ func _on_Area2D_area_exited(_area):
 
 func _physics_process(_delta):
 	if(can_tp and Input.is_action_just_released("up")):
+		yield (play_despawn_animation(), "animation_finished")
 		Teleport()
